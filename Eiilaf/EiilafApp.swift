a9d1e1b4591821c0d33f07fc4d1e73160d27cf8a
EilafMohammad <elafmohammadcs@gmail.com>
2023-10-05 12:37:39 +0300

    Initial Commit

---
+++ Eiilaf/EiilafApp.swift
@@ -0,0 +1,17 @@
+//
+//  EiilafApp.swift
+//  Eiilaf
+//
+//  Created by Eilaf Mohammad on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct EiilafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
